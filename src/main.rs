952330348aca8ce114d6908ac3d62d9bd160e30a
Jordan Elver <jordan.elver@gmail.com>
2016-05-14 22:39:34 +0100

    A terrible first Rust program!
    Basic working request to the Spotify API that returns playlists in JSON
    and prints to the screen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,54 @@
+extern crate hyper;
+
+use std::env;
+use std::io::Read;
+use hyper::Client;
+use hyper::header::{Headers, Authorization, Bearer, ContentType};
+use hyper::mime::{Mime, TopLevel, SubLevel, Attr, Value};
+
+static PLAYLIST_URL: &'static str = "https://api.spotify.com/v1/me/playlists";
+
 fn main() {
-    println!("Hello, world!");
+    let oauth_token = match env::var("OAUTH_TOKEN") {
+      Ok(val) => val,
+      Err(_) => panic!("OAUTH_TOKEN environment variable must be set"),
+    };
+
+    // Headers
+    let mut headers = Headers::new();
+    headers.set(
+        Authorization(
+            Bearer {
+                token: oauth_token.to_owned()
+            }
+        )
+    );
+
+    headers.set(
+      ContentType(
+        Mime(
+          TopLevel::Application,
+          SubLevel::Json,
+          vec![(Attr::Charset, Value::Utf8)]
+        )
+      )
+    );
+
+    println!("{}", headers);
+
+    // Create a request
+    let client = Client::new();
+    let mut res = match client.get(PLAYLIST_URL)
+      .headers(headers)
+      .send() {
+        Ok(res) => res,
+        Err(_) => panic!("Whoops."),
+    };
+
+    // Read the response
+    let mut body = String::new();
+    res.read_to_string(&mut body).unwrap();
+
+    println!("Response: {}", body);
 }
+
